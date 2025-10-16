4d3df53a5314e08338cca7876371d8df24a0a4a1
Mafer <maria.moya@tecsup.edu.pe>
2025-10-15 22:52:44 -0500

    Creacion de las vistas

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# inventory/views.py
 
-# Create your views here.
+from rest_framework import viewsets, filters
+from .models import Categoria, Producto
+from .serializers import CategoriaSerializer, ProductoSerializer
+
+# 📚 CRUD básico de Entidad 2 (Categoría)
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+# 📃 Listado, ➕ Creación, ✏ Edición, ❌ Eliminación y 🔍 Búsqueda de Entidad 1 (Producto)
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    
+    # 🔍 Implementación de búsqueda por filtros
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre'] # Campos por los que se podrá buscar, puedes añadir más
